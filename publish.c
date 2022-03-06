d91e357afdbe64295258f71bea3c092f0f086939
luishwoliveira2016 <luis.biri25@hotmail.com>
2022-03-06 13:22:47 -0300

    Add files via upload

--- publish.c
+++ publish.c
@@ -18,7 +18,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include "MQTTAsync.h"
-
+#include <time.h>
+#include <pthread.h>
 #if !defined(_WIN32)
 #include <unistd.h>
 #else
@@ -28,15 +29,72 @@
 #if defined(_WRS_KERNEL)
 #include <OsWrapper.h>
 #endif
-
+#define GNU_SOURCE
 #define ADDRESS     "tcp://localhost:1883"
-#define CLIENTID    "ExampleClientPub"
-#define TOPIC       "MQTT Examples"
-#define PAYLOAD     "Hello World!"
+//#define CLIENTID    "client2 "
+#define GLOBAL_TOPIC "online"
 #define QOS         1
 #define TIMEOUT     10000L
+#define TAM 50
+
+pthread_t threads[3];
+
+MQTTAsync client;
+MQTTAsync_connectOptions conn_opts = MQTTAsync_connectOptions_initializer;
+int rc;
+
 
 int finished = 0;
+
+char topic_control[50]= "control_"; 
+char client_id[12];
+
+
+MQTTAsync_responseOptions opts = MQTTAsync_responseOptions_initializer;
+
+int messageArrived(void* context, char* topicName, int topicLen, MQTTAsync_message* m);
+
+
+
+void *received(){
+	
+	int count = 0;
+
+	printf("Executando thread...\n");
+	//	MQTTAsync_responseOptions opts = MQTTAsync_responseOptions_initializer;
+
+	MQTTAsync_subscribe(client, "online", QOS, &opts);
+
+	while(1){
+		//printf("teste %d\n",count);
+
+		//count++;
+	}
+}
+
+
+void menu(){
+	
+	int opc;
+
+	printf("1-Listar\n2-Mensagens\n3-sair\n");
+	scanf("%d",&opc);
+
+	switch (opc){
+		case 1:
+			
+			break;
+		
+		case 2:
+			pthread_create(&(threads[1]), NULL,received,NULL);
+			pthread_join(threads[1],NULL);
+		case 3:
+			exit(0) ;
+		
+		default:
+			break;
+	}
+}
 
 void connlost(void *context, char *cause)
 {
@@ -76,7 +134,7 @@ void onSendFailure(void* context, MQTTAsync_failureData* response)
 	int rc;
 
 	printf("Message send failed token %d error code %d\n", response->token, response->code);
-	opts.onSuccess = onDisconnect;
+	//opts.onSuccess = onDisconnect;
 	opts.onFailure = onDisconnectFailure;
 	opts.context = client;
 	if ((rc = MQTTAsync_disconnect(client, &opts)) != MQTTASYNC_SUCCESS)
@@ -93,7 +151,7 @@ void onSend(void* context, MQTTAsync_successData* response)
 	int rc;
 
 	printf("Message with token value %d delivery confirmed\n", response->token);
-	opts.onSuccess = onDisconnect;
+	//opts.onSuccess = onDisconnect;
 	opts.onFailure = onDisconnectFailure;
 	opts.context = client;
 	if ((rc = MQTTAsync_disconnect(client, &opts)) != MQTTASYNC_SUCCESS)
@@ -117,62 +175,87 @@ void onConnect(void* context, MQTTAsync_successData* response)
 	MQTTAsync_responseOptions opts = MQTTAsync_responseOptions_initializer;
 	MQTTAsync_message pubmsg = MQTTAsync_message_initializer;
 	int rc;
-
+	char msg_status[TAM];
+	char msg_conexao[TAM];
+	
 	printf("Successful connection\n");
 	opts.onSuccess = onSend;
 	opts.onFailure = onSendFailure;
 	opts.context = client;
-	pubmsg.payload = PAYLOAD;
-	pubmsg.payloadlen = (int)strlen(PAYLOAD);
 	pubmsg.qos = QOS;
 	pubmsg.retained = 0;
-	if ((rc = MQTTAsync_sendMessage(client, TOPIC, &pubmsg, &opts)) != MQTTASYNC_SUCCESS)
+	/*if ((rc = MQTTAsync_sendMessage(client, TOPIC, &pubmsg, &opts)) != MQTTASYNC_SUCCESS)
 	{
 		printf("Failed to start sendMessage, return code %d\n", rc);
 		exit(EXIT_FAILURE);
-	}
+	}*/
+
+	
+	strcpy(msg_status,client_id);
+	strcat(msg_status," está online");
+
+	//printf("%s\n",msg_status);
+	pubmsg.payload = msg_status;
+	pubmsg.payloadlen = strlen(msg_status);	
+
+	MQTTAsync_sendMessage(client, "online", &pubmsg, &opts);
+
+	strcpy(msg_conexao,client_id);
+	strcat(msg_conexao," deseja enviar mensagem");
+
+
+	pubmsg.payload = msg_conexao;
+	pubmsg.payloadlen =strlen(msg_conexao); 
+	MQTTAsync_sendMessage(client, "client1_control", &pubmsg, &opts);
+
+	MQTTAsync_subscribe(client, "online", QOS, &opts);//envio teste
+	printf("teste chegou até aquiiii hoje....\n");                                                     
+	
 }
 
 int messageArrived(void* context, char* topicName, int topicLen, MQTTAsync_message* m)
 {
 	/* not expecting any messages */
+	printf("teste entrou aqui");
 	return 1;
 }
 
 int main(int argc, char* argv[])
 {
-	MQTTAsync client;
-	MQTTAsync_connectOptions conn_opts = MQTTAsync_connectOptions_initializer;
-	int rc;
+	pthread_create(&(threads[0]),NULL,received,NULL);
+	int timestamp = (int)time(NULL);
+	sprintf(client_id, "%d", timestamp); // converter int para string
+	strcat(topic_control,client_id);
 
-	
 
-	if ((rc = MQTTAsync_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTASYNC_SUCCESS)
+	//printf("topic_control name : %s \n",topic_control);
+	if ((rc = MQTTAsync_create(&client, ADDRESS,client_id , MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTASYNC_SUCCESS)
 	{
 		printf("Failed to create client object, return code %d\n", rc);
 		exit(EXIT_FAILURE);
 	}
 
-	if ((rc = MQTTAsync_setCallbacks(client, NULL, connlost, messageArrived, NULL)) != MQTTASYNC_SUCCESS)
+	if ((rc = MQTTAsync_setCallbacks(client, client, connlost, messageArrived, NULL)) != MQTTASYNC_SUCCESS)
 	{
 		printf("Failed to set callback, return code %d\n", rc);
 		exit(EXIT_FAILURE);
 	}
+
+	printf("Your ID is %s \n",client_id);
+	//menu();
 
 	conn_opts.keepAliveInterval = 20;
 	conn_opts.cleansession = 1;
 	conn_opts.onSuccess = onConnect;
 	conn_opts.onFailure = onConnectFailure;
 	conn_opts.context = client;
+
 	if ((rc = MQTTAsync_connect(client, &conn_opts)) != MQTTASYNC_SUCCESS)
 	{
 		printf("Failed to start connect, return code %d\n", rc);
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Waiting for publication of %s\n"
-         "on topic %s for client with ClientID: %s\n",
-         PAYLOAD, TOPIC, CLIENTID);
 	while (!finished)
 		#if defined(_WIN32)
 			Sleep(100);
